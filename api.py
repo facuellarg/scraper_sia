d422156ee1fea50a515834dbdc9d395ae1b4b13c
Freddy Cuellar <facuellarg@unal.edu.co>
2020-08-24 23:49:38 -0500

    to lower

--- api.py
+++ api.py
@@ -6,10 +6,36 @@ from flask_cors import CORS
 from passlib.hash import sha256_crypt as sh256
 import threading
 import time
-from multiprocessing import Queue, Process
+from queue import Queue
+import re
+from flask.logging import create_logger
+# from multiprocessing import Queue, Process
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
+LOG = create_logger(app)
+
+@app.before_first_request
+def start_thread_cleaner():
+    global jobs,pending_jobs
+    jobs= Queue()
+    pending_jobs = Queue()
+    global threads
+    threads = {}
+    global schedulers
+    schedulers={}
+    global current_jobs
+    current_jobs=0
+    global max_jobs
+    max_jobs=8
+    global lock
+    lock = threading.Lock()
+    global t
+    t = threading.Thread(target=clean_threads)
+    t.daemon=True
+    t.start()
+    threading.Thread(target=manager,args=(jobs,),daemon=True).start()
+
 
 
 
@@ -20,17 +46,20 @@ def response(data,status):
 
 def get_scheduler(user,password,queue):
     user = user.lower()
+    user = user.strip()
+    user = re.sub('@unal.edu.co','', user, re.I|re.A)
     schedule_info=None
     schedule_info = get_scheduler_info(user, password)
     # schedule_info = get_scheduler_info_simulation(user, password)
-    schedulers={user:{}}
+    # schedulers={user:{}}
+    schedulers[user]={}
     if isinstance(schedule_info,dict):
         
         schedulers[user]['password']=sh256.hash(password)
         schedulers[user]['schedule']=schedule_info    
     else:
         schedulers[user]['error']=schedule_info 
-    queue.put(schedulers)
+    queue.put(user)
     
 
 @app.route('/scheduler', methods=['GET'])
@@ -53,11 +82,12 @@ def scheduler():
     if user in threads.keys():
         return response({'message':"Su peticion aun se esta procesando, por favor intente en un minuto"},102)
     else:
+        pending_jobs.put((user,password))
         # threads[user]=threading.Thread(target=get_scheduler, args=(user,password))
         # threads[user].start()
         threads[user]="True"
-        process =Process(target=get_scheduler, args=(user,password,queue))
-        process.start()
+        # process =Process(target=get_scheduler, args=(user,password,queue))
+        # process.start()
         return response({'message':"Su peticion se empezara a procesar, por favor intente en unos minutos"},102)
 
 
@@ -68,14 +98,31 @@ def queue_list():
 
 
 
+@app.route('/', methods=['GET'])
+def clean_threads_query():
+    global t
+    if not t.is_alive():
+        t.start()
+        return{'message':'arrancando limpieza hilos'}
+    return{"message":"Corriendo limpieza de hilos"}
+
+
 def clean_threads():
-    while queue:
-        user = queue.get()
-        user_name = list(user.keys())[0]
-        print("agregando usaurio", user)
-        schedulers[user_name]=user[user_name]
-        print("Reportando: Hilo del usuario ",user_name,' finalizado')
-        del threads[user_name]
+    global current_jobs
+    global threads
+    while True:
+        data = jobs.get()
+        with lock:
+            current_jobs -= 1
+        LOG.debug(f'Reportando: Hilo del usuario {data} finalizado')
+        del threads[data]
+    # while queue:
+    #     user = queue.get()
+    #     user_name = list(user.keys())[0]
+    #     print("agregando usaurio", user)
+    #     schedulers[user_name]=user[user_name]
+    #     print("Reportando: Hilo del usuario ",user_name,' finalizado')
+    #     del threads[user_name]
         # list_keys = list(threads.keys())
         # for thread in list_keys:
         #     if not threads[thread].is_alive():
@@ -84,25 +131,20 @@ def clean_threads():
         # time.sleep(5)
 
 
-@app.route('/', methods=['GET'])
-def clean_threads_query():
-    if not t.is_alive():
-        return{'message':'arrancando limpieza hilos'}
-    return{"message":"Corriendo limpieza de hilos"}
 
-@app.before_first_request
-def start_thread_cleaner():
-    global queue
-    global threads
-    global schedulers
-    threads = {}
-    schedulers={}
-    queue = Queue()
-    global t
-    t = threading.Thread(target=clean_threads)
-    t.daemon=True
-    t.start()
-
+def manager(jobs):
+  global current_jobs
+  global max_jobs
+  global pending_jobs
+  global lock
+  while pending_jobs:
+    task = pending_jobs.get()
+    while(current_jobs == max_jobs):
+      time.sleep(2)
+    LOG.debug(f'entrando al maneger max jobs = {max_jobs} current_jobs={current_jobs}')  
+    threading.Thread(target=get_scheduler ,args=(*task,jobs)).start()
+    current_jobs += 1
+    LOG.logger.debug(f'tareas ejecutandose actualmente {current_jobs}')
 
 if __name__ == '__main__': 
     # t = threading.Thread(target=clean_threads)
