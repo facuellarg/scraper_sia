d3221a86b8757245b3adf96cc2cf2b7376698c19
Freddy Cuellar <facuellarg@unal.edu.co>
2020-08-24 19:07:58 -0500

    multiprocessing added

--- api.py
+++ api.py
@@ -6,41 +6,43 @@ from flask_cors import CORS
 from passlib.hash import sha256_crypt as sh256
 import threading
 import time
+from multiprocessing import Queue, Process
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
 
-schedulers = {}
-threads = {}
+
+
 def response(data,status):
     return {'data':data,'status':status}
 
 
 
-def get_scheduler(user,password):
-    while(True):
-        schedule_info=None
-        try:
-            schedule_info = get_scheduler_info(user, password)
-            # schedule_info = get_scheduler_info_simulation(user, password)
-        except:
-            continue
-        schedulers[user]={}
-        if isinstance(schedule_info,dict):
-            schedulers[user]['password']=sh256.hash(password)
-            schedulers[user]['schedule']=schedule_info    
-        else:
-            schedulers[user]['error']=schedule_info  
-        break
+def get_scheduler(user,password,queue):
+
+    schedule_info=None
+    schedule_info = get_scheduler_info(user, password)
+    # schedule_info = get_scheduler_info_simulation(user, password)
+    schedulers={user:{}}
+    if isinstance(schedule_info,dict):
+        
+        schedulers[user]['password']=sh256.hash(password)
+        schedulers[user]['schedule']=schedule_info    
+    else:
+        schedulers[user]['error']=schedule_info 
+    queue.put(schedulers)
+    
 
 @app.route('/scheduler', methods=['GET'])
 def scheduler():
+    print(schedulers.keys())
     query_parameters = request.args
     if 'user' not in query_parameters or 'password' not in query_parameters:
         return response("bad request", 403)
     user = query_parameters['user']
     password = query_parameters['password']
     if user in schedulers.keys():
+        print("usuario",user,"ya habia sido solicitado")
         if 'error' in schedulers[user].keys():
             del schedulers[user]
             return response("No se pudo obtener la informacion, verifique usuario y contraseña",400)
@@ -51,8 +53,11 @@ def scheduler():
     if user in threads.keys():
         return response({'message':"Su peticion aun se esta procesando, por favor intente en un minuto"},102)
     else:
-        threads[user]=threading.Thread(target=get_scheduler, args=(user,password))
-        threads[user].start()
+        # threads[user]=threading.Thread(target=get_scheduler, args=(user,password))
+        # threads[user].start()
+        threads[user]="True"
+        process =Process(target=get_scheduler, args=(user,password,queue))
+        process.start()
         return response({'message':"Su peticion se empezara a procesar, por favor intente en unos minutos"},102)
 
 
@@ -64,26 +69,43 @@ def queue_list():
 
 
 def clean_threads():
-    while(True):
-        list_keys = list(threads.keys())
-        for thread in list_keys:
-            if not threads[thread].is_alive():
-                print("Reportando: Hilo del usuario ",thread,' finalizado')
-                del threads[thread] 
-        time.sleep(5)
-t = threading.Thread(target=clean_threads)
-t.daemon=True
+    while queue:
+        user = queue.get()
+        user_name = list(user.keys())[0]
+        print("agregando usaurio", user)
+        schedulers[user_name]=user[user_name]
+        print("Reportando: Hilo del usuario ",user_name,' finalizado')
+        del threads[user_name]
+        # list_keys = list(threads.keys())
+        # for thread in list_keys:
+        #     if not threads[thread].is_alive():
+        #         print("Reportando: Hilo del usuario ",thread,' finalizado')
+        #         del threads[thread] 
+        # time.sleep(5)
+
 
 @app.route('/', methods=['GET'])
 def clean_threads_query():
     if not t.is_alive():
-        t.start()
+        return{'message':'arrancando limpieza hilos'}
     return{"message":"Corriendo limpieza de hilos"}
 
-
-
-if __name__ == '__main__': 
+@app.before_first_request
+def start_thread_cleaner():
+    global queue
+    global threads
+    global schedulers
+    threads = {}
+    schedulers={}
+    queue = Queue()
+    global t
     t = threading.Thread(target=clean_threads)
     t.daemon=True
     t.start()
+
+
+if __name__ == '__main__': 
+    # t = threading.Thread(target=clean_threads)
+    # t.daemon=True
+    # t.start()
     app.run(debug=True)
--- scheduler.py
+++ scheduler.py
@@ -100,105 +100,112 @@ def get_scheduler_info(user,password):
     GROUP=10
     PROFESSOR = 11
     colors = Bold_10.hex_colors
-    # --------------------------- para heroku-----------------------------------------------------------------
-    op = webdriver.ChromeOptions()
-    op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    op.add_argument("--headless")
-    op.add_argument("--disable-dev-shm-usage")
-    op.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=op)
-    # -------------------------------------------------------------------------------------------------------
-    # ---------------------------- para local-----------------------------------------------------------------
-    # driver = webdriver.Chrome(ChromeDriverManager().install())
-    # driver.minimize_window()
-    # -------------------------------------------------------------------------------------------------------
-    driver.get(login_url)
-    print(driver)
-    login_sia(driver, user, password)
-    if go_to_horario(driver):
-        # if user in schedulers.keys():
-        #     return schedulers[user]
-        go_to_list(driver)
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.XPATH,'//tr[contains(@class,"af_calendar_list-row")]'))
-        )
+    times_closed= 0
+    i=0
+    while(times_closed < 10):
+        # --------------------------- para heroku-----------------------------------------------------------------
+        op = webdriver.ChromeOptions()
+        op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        op.add_argument("--headless")
+        op.add_argument("--disable-dev-shm-usage")
+        op.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=op)
+        # -------------------------------------------------------------------------------------------------------
+        # ---------------------------- para local-----------------------------------------------------------------
+        # driver = webdriver.Chrome(ChromeDriverManager().install())
+        # driver.minimize_window()
+        # -------------------------------------------------------------------------------------------------------
+        driver.get(login_url)
         info_courses={}
-        courses_len = len(driver.find_elements(By.XPATH,'//tr[contains(@class,"af_calendar_list-row")]'))
-        current_day = ""
-        i = 0
-        while i < courses_len:
-            load = False
-            tries = 0
-            while not load:
-                tries +=1
-                if tries > 5:
-                    # print(i)
-                    driver.refresh()
-                    go_to_horario(driver)
-                    go_to_list(driver,1)
-                    WebDriverWait(driver, 20).until(
-                        EC.presence_of_element_located((By.XPATH,'//tr[contains(@class,"af_calendar_list-row")]'))
-                    )
+        try:
+            login_sia(driver, user, password)
+            if go_to_horario(driver):
+                go_to_list(driver)
+                WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located((By.XPATH,'//tr[contains(@class,"af_calendar_list-row")]'))
+                )
+                
+                courses_len = len(driver.find_elements(By.XPATH,'//tr[contains(@class,"af_calendar_list-row")]'))
+                current_day = ""
+                while i < courses_len:
+                    load = False
                     tries = 0
-                    continue
-                courses = driver.find_elements(By.XPATH,'//tr[contains(@class,"af_calendar_list-row")]')
-                # print("i antes de asignar course",i)
-                course = courses[i]
-                day = course.find_elements(By.XPATH,'.//th[@class="af_calendar_list-day-of-week-column af_calendar_list-cell"]')
-                if len(day) !=0:
-                    current_day = day[0].text.strip()
+                    while not load:
+                        tries +=1
+                        if tries > 2:
+                            # print(i)
+                            driver.refresh()
+                            go_to_horario(driver)
+                            go_to_list(driver,1)
+                            WebDriverWait(driver, 20).until(
+                                EC.presence_of_element_located((By.XPATH,'//tr[contains(@class,"af_calendar_list-row")]'))
+                            )
+                            tries = 0
+                            
+                            continue
+                        courses = driver.find_elements(By.XPATH,'//tr[contains(@class,"af_calendar_list-row")]')
+                        course = courses[i]
+                        day = course.find_elements(By.XPATH,'.//th[@class="af_calendar_list-day-of-week-column af_calendar_list-cell"]')
+                        if len(day) !=0:
+                            current_day = day[0].text.strip()
 
-                courses = driver.find_elements(By.XPATH,'//a[contains(@class,"af_calendar_list-title-link")]')
-                course = courses[i]
-                course.click()
+                        courses = driver.find_elements(By.XPATH,'//a[contains(@class,"af_calendar_list-title-link")]')
+                        course = courses[i]
+                        time.sleep(1)
+                        course.click()
 
-                time.sleep(2)
-                container = WebDriverWait(driver, 20).until(
-                    EC.presence_of_element_located((By.XPATH,'//div[@data-afr-fid="f1"]'))
-                )
-                name =None
-                try:
-                    name = container.find_element(By.XPATH,'.//div[@class="af_dialog_title"]')
-                except:
-                    continue
-                if name!= None:
-                    spans = container.find_elements(By.XPATH, './/td[@class="af_dialog_content"]//span')
-                    #i = 0
-                    professor = None
-                    if len(spans)>11:
-                        professor = spans[PROFESSOR].text
-                    hour = clear_hour(spans[HOUR].text)
-                    classroom = clear_classroom(spans[CLASSROOM].text)
-                    group = clear_group(spans[GROUP].text)
-                    name_splited = name.text.split(' ')
-                    code = name_splited[0]
-                    name_course = ' '.join(name_splited[1:])
-                    info_course =   {'name':name_course,
-                                    'hour':hour,
-                                    'group':group,
-                                    'classroom':classroom,
-                                    'professor':professor}
-                    if code not in info_courses.keys():
-                        info_courses[code]={}
-                        info_courses[code]['color'] = random_color(colors)
-                    if current_day in info_courses[code].keys():
-                        info_courses[code][current_day] = update_course( info_courses[code][current_day],info_course)
-                    else:
-                        info_courses[code][current_day]=info_course
-                button = WebDriverWait(container,20).until(
-                    EC.presence_of_element_located((By.XPATH, '//button[@class="af_dialog_footer-button-affirmative af_dialog_footer-button p_AFTextOnly"]'))
-                )
-                try:
-                    button.click()
-                except:
-                    continue
-                load = True
-                i += 1
-        driver.quit()
-        # schedulers[user]=info_courses
-        return info_courses
-    return("Usuario o contraseña invalidos")
-
+                        time.sleep(2)
+                        container = WebDriverWait(driver, 20).until(
+                            EC.presence_of_element_located((By.XPATH,'//div[@data-afr-fid="f1"]'))
+                        )
+                        name =None
+                        try:
+                            name = container.find_element(By.XPATH,'.//div[@class="af_dialog_title"]')
+                        except:
+                            continue
+                        if name!= None:
+                            spans = container.find_elements(By.XPATH, './/td[@class="af_dialog_content"]//span')
+                            #i = 0
+                            professor = None
+                            if len(spans)>11:
+                                professor = spans[PROFESSOR].text
+                            hour = clear_hour(spans[HOUR].text)
+                            classroom = clear_classroom(spans[CLASSROOM].text)
+                            group = clear_group(spans[GROUP].text)
+                            name_splited = name.text.split(' ')
+                            code = name_splited[0]
+                            name_course = ' '.join(name_splited[1:])
+                            info_course =   {'name':name_course,
+                                            'hour':hour,
+                                            'group':group,
+                                            'classroom':classroom,
+                                            'professor':professor}
+                            if code not in info_courses.keys():
+                                info_courses[code]={}
+                                info_courses[code]['color'] = random_color(colors)
+                            if current_day in info_courses[code].keys():
+                                info_courses[code][current_day] = update_course( info_courses[code][current_day],info_course)
+                            else:
+                                info_courses[code][current_day]=info_course
+                        button = WebDriverWait(container,20).until(
+                            EC.presence_of_element_located((By.XPATH, '//button[@class="af_dialog_footer-button-affirmative af_dialog_footer-button p_AFTextOnly"]'))
+                        )
+                        try:
+                            button.click()
+                        except:
+                            continue
+                        load = True
+                        i += 1
+                driver.quit()
+                # schedulers[user]=info_courses
+                return info_courses
+            return("Usuario o contraseña invalidos")
+        except:
+            driver.quit()
+            times_closed += 1
+            continue
+    return("No se pudo traer la informacion")
+        
 def get_scheduler_info_simulation(user,password):
     time.sleep(random.randint(0,30))
     if user in schedulers.keys():
